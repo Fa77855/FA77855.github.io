ef3424434a0d191767514aaca3968d92a8a35bbd
Fa77855 <60405216+Fa77855@users.noreply.github.com>
2020-04-19 15:32:36 -0400

    Add files via upload

---
+++ NewWebMap/qgis2web_2020_04_19-15_30_31_588277/layers/layers.js
@@ -0,0 +1,42 @@
+var wms_layers = [];
+
+
+        var lyr_CartoLight_0 = new ol.layer.Tile({
+            'title': 'Carto Light',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'http://a.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'
+            })
+        });
+var format_WebMapPoints_1 = new ol.format.GeoJSON();
+var features_WebMapPoints_1 = format_WebMapPoints_1.readFeatures(json_WebMapPoints_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_WebMapPoints_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_WebMapPoints_1.addFeatures(features_WebMapPoints_1);
+var lyr_WebMapPoints_1 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_WebMapPoints_1, 
+                style: style_WebMapPoints_1,
+                interactive: true,
+    title: 'WebMapPoints<br />\
+    <img src="styles/legend/WebMapPoints_1_0.png" /> 1.0000 - 1.0000<br />\
+    <img src="styles/legend/WebMapPoints_1_1.png" /> 1.0000 - 3.0000<br />\
+    <img src="styles/legend/WebMapPoints_1_2.png" /> 3.0000 - 5.0000<br />\
+    <img src="styles/legend/WebMapPoints_1_3.png" /> 5.0000 - 9.0000<br />\
+    <img src="styles/legend/WebMapPoints_1_4.png" /> 9.0000 - 19.0000<br />'
+        });
+
+lyr_CartoLight_0.setVisible(true);lyr_WebMapPoints_1.setVisible(true);
+var layersList = [lyr_CartoLight_0,lyr_WebMapPoints_1];
+lyr_WebMapPoints_1.set('fieldAliases', {'Number of Females': 'Number of Females', 'Number of Males': 'Number of Males', 'Total Number': 'Total Number', 'Latitude': 'Latitude', 'Longitude': 'Longitude', });
+lyr_WebMapPoints_1.set('fieldImages', {'Number of Females': 'Range', 'Number of Males': 'Range', 'Total Number': 'Range', 'Latitude': 'TextEdit', 'Longitude': 'TextEdit', });
+lyr_WebMapPoints_1.set('fieldLabels', {'Number of Females': 'inline label', 'Number of Males': 'inline label', 'Total Number': 'inline label', 'Latitude': 'inline label', 'Longitude': 'inline label', });
+lyr_WebMapPoints_1.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
